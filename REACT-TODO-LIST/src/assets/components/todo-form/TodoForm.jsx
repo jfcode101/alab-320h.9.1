87d655e64e16a80d3bf77f2488ff0b050b422f0f
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-10-01 17:02:23 -0400

    update TodoForm.jsx

--- REACT-TODO-LIST/src/assets/components/todo-form/TodoForm.jsx
+++ REACT-TODO-LIST/src/assets/components/todo-form/TodoForm.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 function TodoForm({ dispatch }) {
   // state to store the input value
-  const [inputVal, setVal] = useState();
+  const [inputVal, setInputVal] = useState("");
 
   // function to handle form submission
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    // preevent auto submission
+    e.preventDefault();
+    if (inputVal.trim()) {
+      dispatch({ type: "ADD_TASK", title: inputVal });
+      // clear the input fiel
+      setInputVal("");
+    }
+  };
 
   return (
-    <form className="cls-form">
-      <input type="text" value="" placeholder="What do you need to do " />
-      <button type="submit">Add Todo</button>
+    <form onSubmit={handleSubmit} className="cls-form">
+      <input
+        type="text"
+        value={inputVal}
+        // add an onchange event listener
+        onChange={(e) => setInputVal(e.target.value)}
+        placeholder="Add a task to you Todo list"
+      />
+      <button type="submit">Add Task</button>
     </form>
   );
 }
